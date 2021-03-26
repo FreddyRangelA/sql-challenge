77685e825b21b29f631c2619def578b345383918
freddy31_ag@hotmail.com <freddy31_ag@hotmail.com>
2021-03-26 02:32:03 -0500

    1-6 tables

---
+++ EmployeeSQL/.ipynb_checkpoints/QuickDBD-Free Diagram-checkpoint.sql
@@ -0,0 +1,124 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/wUbWTP
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+-- Customer
+-- -
+-- CustomerID PK int
+-- Name string INDEX
+-- Address1 string
+-- Address2 NULL string
+-- Address3 NULL string
+-- Order
+-- -
+-- OrderID PK int
+-- CustomerID int FK >- Customer.CustomerID
+-- TotalAmount money
+-- OrderStatusID int FK >- os.OrderStatusID
+-- OrderLine as ol
+-- ----
+-- OrderLineID PK int
+-- OrderID int FK >- Order.OrderID
+-- ProductID int FK >- p.ProductID
+-- Quantity int
+-- Product as p
+-- ------------
+-- ProductID PK int
+-- Name varchar(200) UNIQUE
+-- Price money
+-- OrderStatus as os
+-- ----
+-- OrderStatusID PK int
+-- Name UNIQUE string
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [deparments] (
+    [dept_no] string  NOT NULL ,
+    [dept_name] strin  NULL ,
+    CONSTRAINT [PK_deparments] PRIMARY KEY CLUSTERED (
+        [dept_no] ASC
+    )
+)
+
+CREATE TABLE [dept_emp] (
+    [emp_no] int  NOT NULL ,
+    [dept_no] string  NOT NULL ,
+    CONSTRAINT [PK_dept_emp] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC,[dept_no] ASC
+    )
+)
+
+CREATE TABLE [dept_manager] (
+    [dept_no] string  NOT NULL ,
+    [emp_no] int  NOT NULL ,
+    CONSTRAINT [PK_dept_manager] PRIMARY KEY CLUSTERED (
+        [dept_no] ASC
+    )
+)
+
+CREATE TABLE [employees] (
+    [emp_no] int  NOT NULL ,
+    [emp_title_id] string  NOT NULL ,
+    [birth_date] int  NOT NULL ,
+    [first_name] string  NOT NULL ,
+    [last_name] string  NOT NULL ,
+    [sex] string  NOT NULL ,
+    [hire_date] int  NOT NULL ,
+    CONSTRAINT [PK_employees] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
+
+CREATE TABLE [salaries] (
+    [emp_no] int  NOT NULL ,
+    [salary] int  NOT NULL ,
+    CONSTRAINT [PK_salaries] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
+
+CREATE TABLE [titles] (
+    [title_id] strin  NOT NULL ,
+    [title] string  NOT NULL ,
+    CONSTRAINT [PK_titles] PRIMARY KEY CLUSTERED (
+        [title_id] ASC
+    )
+)
+
+ALTER TABLE [deparments] WITH CHECK ADD CONSTRAINT [FK_deparments_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [dept_manager] ([dept_no])
+
+ALTER TABLE [deparments] CHECK CONSTRAINT [FK_deparments_dept_no]
+
+ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [dept_emp] CHECK CONSTRAINT [FK_dept_emp_emp_no]
+
+ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [deparments] ([dept_no])
+
+ALTER TABLE [dept_emp] CHECK CONSTRAINT [FK_dept_emp_dept_no]
+
+ALTER TABLE [dept_manager] WITH CHECK ADD CONSTRAINT [FK_dept_manager_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [dept_manager] CHECK CONSTRAINT [FK_dept_manager_emp_no]
+
+ALTER TABLE [employees] WITH CHECK ADD CONSTRAINT [FK_employees_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [salaries] ([emp_no])
+
+ALTER TABLE [employees] CHECK CONSTRAINT [FK_employees_emp_no]
+
+ALTER TABLE [employees] WITH CHECK ADD CONSTRAINT [FK_employees_emp_title_id] FOREIGN KEY([emp_title_id])
+REFERENCES [titles] ([title_id])
+
+ALTER TABLE [employees] CHECK CONSTRAINT [FK_employees_emp_title_id]
+
+COMMIT TRANSACTION QUICKDBD
---
+++ EmployeeSQL/QuickDBD-Free Diagram (2).sql
@@ -0,0 +1,118 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/wUbWTP
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+DROP TABLE IF EXISTS deparments;
+CREATE TABLE "deparments" (
+    -- several department can be under one manage, therefore, one to many.
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NULL,
+    CONSTRAINT "pk_deparments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    -- one to one relation.
+    "emp_no" int   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    -- one to one relation.
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    -- only one employee can have one salary.
+    "emp_no" int   NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "deparments" ADD CONSTRAINT "fk_deparments_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "dept_manager" ("dept_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "deparments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "dept_emp" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "salaries" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+--1.List of details for employees.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex,salaries.salary FROM employees
+JOIN salaries
+ON employees.emp_no=salaries.emp_no;
+
+--2.List with details from employees hired in 1986.
+SELECT employees.first_name, employees.last_name, employees.hire_date FROM employees
+WHERE hire_date BETWEEN '1986-01-01'AND '1987-01-01';
+
+--3.Linst of manager for each deparment.
+SELECT * FROM deparments;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+ 
+SELECT deparments.dept_no, deparments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name FROM deparments
+JOIN dept_manager
+ON deparments.dept_no = dept_manager.dept_no
+JOIN employees
+ON employees.emp_no = dept_manager.emp_no;
+
+--4.list the deparment for each employee.
+SELECT employees.emp_no,employees.last_name, employees.first_name,deparments.dept_name FROM employees
+JOIN dept_emp
+on employees.emp_no=dept_emp.emp_no
+JOIN deparments
+on dept_emp.dept_no=deparments.dept_no;
+
+--5.List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT employees.first_name, employees.last_name, employees.sex FROM employees
+WHERE employees.first_name = 'Hercules'
+AND employees.last_name LIKE 'B%';
+
+
+
+
